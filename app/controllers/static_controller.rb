2d9a62831a71889991a76b8e47f86811f7896ae7
PStanos <paulstanos@gmail.com>
2015-03-16 16:00:39 -0400

    Tutorial complete

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,6 +6,6 @@ class StaticController < ApplicationController
   end
 
   def cats
-    @cats = CatAPI.new.get_images(results_per_page: 10)
+    @cats = CatAPI.new.get_images(results_per_page: params[:number_of_cats] || 1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Project9::Application.routes.draw do
   root 'static#home'
   get '/about' => 'static#about'
-  get '/cat-pictures'                 => 'static#cats'
-  get '/cat-pictures/:number_of_cats' => 'static#cats'
+  get '/cat-pictures(/:number_of_cats)' => 'static#cats', as: :cat_pictures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
